0269976b2cdf49870817ed6b4b6c57f03a486132
gabrielissantos <gabrielisantos17.gs@gmail.com>
2018-12-04 09:37:38 -0200

    telas.xml

---
+++ app/src/main/java/com/example/gabrielisantos/p2_mobile/cocktail_item.kt
@@ -0,0 +1,12 @@
+package com.example.gabrielisantos.p2_mobile
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class cocktail_item : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cocktail_item)
+    }
+}
